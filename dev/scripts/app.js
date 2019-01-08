6fc6ba082f5bc59fc26846fdd727bad96849b18d
Aaron Janke <janke.aaron@gmail.com>
2019-01-08 15:15:56 -0500

    update

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -7,101 +7,101 @@ import axios from 'axios';
 //NOTE ON RESPONSIVENESS: the canvas will automatically resize to the current browser size if you refresh the browser.
 
 class App extends React.Component {
-  constructor() {
-    super()
+	constructor() {
+		super()
 
-    this.state = {
-      sizeValue: 5,
-      controlsHidden: false,
-      color: "red",
-      lightsColor: "white",
-      vertices: 0,
-      middleBulbSpeed: 0.004,
-      topBulbSpeed: 0.003,
-      bottomBulbSpeed: 0.005,
-      middleBulbLum: 300,
-      topBulbLum: 300,
-      bottomBulbLum: 300,
-    }   
+		this.state = {
+			sizeValue: 5,
+			controlsHidden: false,
+			color: "red",
+			lightsColor: "white",
+			vertices: 0,
+			middleBulbSpeed: 0.004,
+			topBulbSpeed: 0.003,
+			bottomBulbSpeed: 0.005,
+			middleBulbLum: 300,
+			topBulbLum: 300,
+			bottomBulbLum: 300,
+		}   
 
-    // this.sizeChange = this.sizeChange.bind(this);
-    this.getRandomColor = this.getRandomColor.bind(this);
-    this.getRandomLightsColor = this.getRandomLightsColor.bind(this);
-    this.toggleControls = this.toggleControls.bind(this);
-    // this.verticesChange = this.verticesChange.bind(this);
-    // this.middleBulbSpeedChange = this.middleBulbSpeedChange.bind(this);
-    // this.topBulbSpeedChange = this.topBulbSpeedChange.bind(this);
-    // this.bottomBulbSpeedChange = this.bottomBulbSpeedChange.bind(this);
-  }
+		// this.sizeChange = this.sizeChange.bind(this);
+		this.getRandomColor = this.getRandomColor.bind(this);
+		this.getRandomLightsColor = this.getRandomLightsColor.bind(this);
+		this.toggleControls = this.toggleControls.bind(this);
+		// this.verticesChange = this.verticesChange.bind(this);
+		// this.middleBulbSpeedChange = this.middleBulbSpeedChange.bind(this);
+		// this.topBulbSpeedChange = this.topBulbSpeedChange.bind(this);
+		// this.bottomBulbSpeedChange = this.bottomBulbSpeedChange.bind(this);
+	}
 
 
 
-  //FUNCTIONS FOR CHANGING THE ANIMATION
+	//FUNCTIONS FOR CHANGING THE ANIMATION
 
-  getRandomColor() {
-    axios.get('https://fun-fun-colors.herokuapp.com/randomcolor')
-    .then((res) => {
-      this.setState({color: res.data.color});
-    })
-  }
+	getRandomColor() {
+		axios.get('https://fun-fun-colors.herokuapp.com/randomcolor')
+		.then((res) => {
+			this.setState({color: res.data.color});
+		})
+	}
 
-  getRandomLightsColor() {
-    axios.get('https://fun-fun-colors.herokuapp.com/randomcolor')
-    .then((res) => {
-      this.setState({lightsColor: res.data.color});
-    })
-  }
+	getRandomLightsColor() {
+		axios.get('https://fun-fun-colors.herokuapp.com/randomcolor')
+		.then((res) => {
+			this.setState({lightsColor: res.data.color});
+		})
+	}
 
-  //FUNCTION FOR TOGGLING THE VISIBILITY OF THE CONTROL BOX
-  toggleControls(e) {
-    e.preventDefault();
-    this.setState({controlsHidden: !this.state.controlsHidden});
-  }
+	//FUNCTION FOR TOGGLING THE VISIBILITY OF THE CONTROL BOX
+	toggleControls(e) {
+		e.preventDefault();
+		this.setState({controlsHidden: !this.state.controlsHidden});
+	}
 
-  render() {
-    return (
-      <div className="mainDiv" >
-        { this.state.controlsHidden ? <a className="hamburgerIcon" onClick={this.toggleControls} href=""><i  className="fas fa-bars fa-2x"></i></a>
-        : null}
-        <Controls 
-          vertices={this.state.vertices} 
-          verticesChange={(e) => this.setState({ vertices: e.target.value })} 
-          controlsHidden={this.state.controlsHidden} 
-          toggleControls={this.toggleControls} 
-          sizeValue={this.state.sizeValue} 
-          sizeChange={(e) => this.setState({ sizeValue: e.target.value })} 
-          getRandomColor={this.getRandomColor} 
-          color={this.state.color} 
-          getRandomLightsColor={this.getRandomLightsColor} 
-          lightsColor={this.state.lightsColor} 
-          middleBulbSpeed={this.state.middleBulbSpeed}
-          topBulbSpeed={this.state.topBulbSpeed}
-          bottomBulbSpeed={this.state.bottomBulbSpeed}
-          topBulbSpeedChange={(e) => this.setState({ topBulbSpeed: e })}
-          middleBulbSpeedChange={(e) => this.setState({ middleBulbSpeed: e })}
-          bottomBulbSpeedChange={(e) => this.setState({ bottomBulbSpeed: e })}
-          middleBulbLum={this.state.middleBulbLum}
-          topBulbLum={this.state.topBulbLum}
-          bottomBulbLum={this.state.bottomBulbLum}
-          topBulbLumChange={(e) => this.setState({ topBulbLum: e })}
-          middleBulbLumChange={(e) => this.setState({ middleBulbLum: e })}
-          bottomBulbLumChange={(e) => this.setState({ bottomBulbLum: e })}
-        />
-        <Shape 
-          lightsColor={this.state.lightsColor} 
-          color={this.state.color} 
-          sizeValue={this.state.sizeValue} 
-          vertices={this.state.vertices} 
-          middleBulbSpeed={this.state.middleBulbSpeed}
-          topBulbSpeed={this.state.topBulbSpeed}
-          bottomBulbSpeed={this.state.bottomBulbSpeed}
-          middleBulbLum={this.state.middleBulbLum}
-          topBulbLum={this.state.topBulbLum}
-          bottomBulbLum={this.state.bottomBulbLum}
-        />
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div className="mainDiv" >
+				{ this.state.controlsHidden ? <a className="hamburgerIcon" onClick={this.toggleControls} href=""><i  className="fas fa-bars fa-2x"></i></a>
+				: null}
+				<Controls 
+					vertices={this.state.vertices} 
+					verticesChange={(e) => this.setState({ vertices: e.target.value })} 
+					controlsHidden={this.state.controlsHidden} 
+					toggleControls={this.toggleControls} 
+					sizeValue={this.state.sizeValue} 
+					sizeChange={(e) => this.setState({ sizeValue: e.target.value })} 
+					getRandomColor={this.getRandomColor} 
+					color={this.state.color} 
+					getRandomLightsColor={this.getRandomLightsColor} 
+					lightsColor={this.state.lightsColor} 
+					middleBulbSpeed={this.state.middleBulbSpeed}
+					topBulbSpeed={this.state.topBulbSpeed}
+					bottomBulbSpeed={this.state.bottomBulbSpeed}
+					topBulbSpeedChange={(e) => this.setState({ topBulbSpeed: e })}
+					middleBulbSpeedChange={(e) => this.setState({ middleBulbSpeed: e })}
+					bottomBulbSpeedChange={(e) => this.setState({ bottomBulbSpeed: e })}
+					middleBulbLum={this.state.middleBulbLum}
+					topBulbLum={this.state.topBulbLum}
+					bottomBulbLum={this.state.bottomBulbLum}
+					topBulbLumChange={(e) => this.setState({ topBulbLum: e })}
+					middleBulbLumChange={(e) => this.setState({ middleBulbLum: e })}
+					bottomBulbLumChange={(e) => this.setState({ bottomBulbLum: e })}
+				/>
+				<Shape 
+					lightsColor={this.state.lightsColor} 
+					color={this.state.color} 
+					sizeValue={this.state.sizeValue} 
+					vertices={this.state.vertices} 
+					middleBulbSpeed={this.state.middleBulbSpeed}
+					topBulbSpeed={this.state.topBulbSpeed}
+					bottomBulbSpeed={this.state.bottomBulbSpeed}
+					middleBulbLum={this.state.middleBulbLum}
+					topBulbLum={this.state.topBulbLum}
+					bottomBulbLum={this.state.bottomBulbLum}
+				/>
+			</div>
+		)
+	}
 }
 
 ReactDOM.render(<App/>, document.getElementById('app'));
--- dev/scripts/shape.js
+++ dev/scripts/shape.js
@@ -4,347 +4,228 @@ import * as THREE from 'three';
 // import OBJLoader from 'three-obj-loader';
 
 class Shape extends React.Component {
-   constructor() {
-      super()
+	constructor() {
+		super()
 
-      this.state = {
-         sizeValue: 5,
-         color: "red",
-         lightsColor: "white",
-         vertices: 0,
-         shapeRotationX : 0,
-         shapeRotationY : 0,
-         middleBulbDistance : 0,
-         bottomBulbDistance : 0,
-         topBulbDistance : 0,
+		this.state = {
+			sizeValue: 5,
+			color: "red",
+			lightsColor: "white",
+			vertices: 0,
+			shapeRotationX : 0,
+			shapeRotationY : 0,
+			middleBulbDistance : 0,
+			bottomBulbDistance : 0,
+			topBulbDistance : 0,
 
-         middleBulbSpeed : 0.004,
-         topBulbSpeed: 0.003,
-         bottomBulbSpeed: 0.005,
-         middleBulbLum : 300,
-         topBulbLum: 300,
-         bottomBulbLum: 300,
-         // middleBulbPositionX : 0,
-         // middleBulbPositionZ : 0,         
-         // bottomBulbPositionX : 0,
-         // bottomBulbPositionZ : 0,         
-         // topBulbPositionX : 0,
-         // topBulbPositionZ : 0,
-         
-      }
+			middleBulbSpeed : 0.004,
+			topBulbSpeed: 0.003,
+			bottomBulbSpeed: 0.005,
+			middleBulbLum : 300,
+			topBulbLum: 300,
+			bottomBulbLum: 300,
+		}
 
-      this.createObject = this.createObject.bind(this)
-      this.start = this.start.bind(this)
-      this.stop = this.stop.bind(this)
-      this.animate = this.animate.bind(this)
-      this.renderScene = this.renderScene.bind(this)
-   };
+		this.createObject = this.createObject.bind(this)
+		this.start = this.start.bind(this)
+		this.stop = this.stop.bind(this)
+		this.animate = this.animate.bind(this)
+		this.renderScene = this.renderScene.bind(this)
+	};
 
-   componentDidMount() {
-      const renderer = new THREE.WebGLRenderer();
-      document.body.appendChild(renderer.domElement);
-      renderer.setSize(window.innerWidth, window.innerHeight);
-      this.renderer = renderer
+	componentDidMount() {
+		const renderer = new THREE.WebGLRenderer();
+		document.body.appendChild(renderer.domElement);
+		renderer.setSize(window.innerWidth, window.innerHeight);
+		this.renderer = renderer
 
-      this.createObject();
-   };
+		this.createObject();
+	};
 
-   createObject() {
+	createObject() {
 
-      //STATES IMPORTED
-      let { sizeValue, color, vertices, lightsColor, middleBulbLum, topBulbLum, bottomBulbLum } = this.state;
+		//STATES IMPORTED
+		let { sizeValue, color, vertices, lightsColor, middleBulbLum, topBulbLum, bottomBulbLum } = this.state;
 
-      let scene = new THREE.Scene();
+		let scene = new THREE.Scene();
 
-      //CAMERA
-      let camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-      camera.position.set( 0, 0, 25 );
+		//CAMERA
+		let camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+		camera.position.set( 0, 0, 25 );
 
-      //SHAPE
-      let geometry = new THREE.TetrahedronGeometry(sizeValue, vertices);
-      let material = new THREE.MeshStandardMaterial({ 
-         color: color,
-         metalness: 0.5,
-         roughness: 0.7
-      });
-      let shape = new THREE.Mesh(geometry, material);
+		//SHAPE
+		let geometry = new THREE.TetrahedronGeometry(sizeValue, vertices);
+		let material = new THREE.MeshStandardMaterial({ 
+			color: color,
+			metalness: 0.5,
+			roughness: 0.7
+		});
+		let shape = new THREE.Mesh(geometry, material);
 
 
-      //ALL BULBS
-      let bulbGeometry = new THREE.SphereGeometry( 0.08, 16, 8 );
-      let bulbMat = new THREE.MeshStandardMaterial( {
-         emissive: lightsColor,
-         emissiveIntensity: 1,
-         color: lightsColor
-      });
+		//ALL BULBS
+		let bulbGeometry = new THREE.SphereGeometry( 0.08, 16, 8 );
+		let bulbMat = new THREE.MeshStandardMaterial( {
+			emissive: lightsColor,
+			emissiveIntensity: 1,
+			color: lightsColor
+		});
 
-      //BULB 1
-      let MiddleBulb = new THREE.PointLight( lightsColor, 1, middleBulbLum, 20 );
-      MiddleBulb.add( new THREE.Mesh( bulbGeometry, bulbMat ) );
-      MiddleBulb.position.set( -7.5, 0, 10 );
+		//BULB 1
+		let MiddleBulb = new THREE.PointLight( lightsColor, 1, middleBulbLum, 20 );
+		MiddleBulb.add( new THREE.Mesh( bulbGeometry, bulbMat ) );
+		MiddleBulb.position.set( -7.5, 0, 10 );
 
-      //BULB 2
-      let BottomBulb = new THREE.PointLight( lightsColor, 1, bottomBulbLum, 20 );
-      BottomBulb.add( new THREE.Mesh( bulbGeometry, bulbMat ) );
-      BottomBulb.position.set( 0, -9, 0 );      
-      
-      //BULB 3
-      let TopBulb = new THREE.PointLight(lightsColor, 1, topBulbLum, 20 );
-      TopBulb.add( new THREE.Mesh( bulbGeometry, bulbMat ) );
-      TopBulb.position.set( 0, 9, 0 );     
+		//BULB 2
+		let BottomBulb = new THREE.PointLight( lightsColor, 1, bottomBulbLum, 20 );
+		BottomBulb.add( new THREE.Mesh( bulbGeometry, bulbMat ) );
+		BottomBulb.position.set( 0, -9, 0 );      
+		
+		//BULB 3
+		let TopBulb = new THREE.PointLight(lightsColor, 1, topBulbLum, 20 );
+		TopBulb.add( new THREE.Mesh( bulbGeometry, bulbMat ) );
+		TopBulb.position.set( 0, 9, 0 );     
 
-      //GENERAL POINTLIGHT
-      let pointLight = new THREE.PointLight(0xffffff, 1, 60, 5);
-      pointLight.position.set(0, 0, 11);
+		//GENERAL POINTLIGHT
+		let pointLight = new THREE.PointLight(0xffffff, 1, 60, 5);
+		pointLight.position.set(0, 0, 11);
 
-      //FLOOR
-      let floorMat = new THREE.MeshStandardMaterial( {
-         // shininess: 100,
-         side: THREE.DoubleSide,
-         roughness: 0.6,
-         color: "white",
-         metalness: 1,
-         // bumpScale: 0.0005
-      });
-      let floorGeometry = new THREE.PlaneBufferGeometry( 900, 900, 8, 8 );
-      let floorMesh = new THREE.Mesh( floorGeometry, floorMat );
-      floorMesh.rotation.x = -Math.PI / 2.0;
-      floorMesh.position.set( 0, -10, 0 );
+		//FLOOR
+		let floorMat = new THREE.MeshStandardMaterial( {
+			// shininess: 100,
+			side: THREE.DoubleSide,
+			roughness: 0.6,
+			color: "white",
+			metalness: 1,
+			// bumpScale: 0.0005
+		});
+		let floorGeometry = new THREE.PlaneBufferGeometry( 900, 900, 8, 8 );
+		let floorMesh = new THREE.Mesh( floorGeometry, floorMat );
+		floorMesh.rotation.x = -Math.PI / 2.0;
+		floorMesh.position.set( 0, -10, 0 );
 
-      //FLOOR2 (CEILING)
-      let floorMesh2 = new THREE.Mesh( floorGeometry, floorMat );
-      floorMesh2.rotation.x = -Math.PI / 2.0;
-      floorMesh2.position.set( 0, 10, 0 );
+		//FLOOR2 (CEILING)
+		let floorMesh2 = new THREE.Mesh( floorGeometry, floorMat );
+		floorMesh2.rotation.x = -Math.PI / 2.0;
+		floorMesh2.position.set( 0, 10, 0 );
 
-      // let loader = new THREE.OBJLoader();
-      // loader.load(
-      //    // resource URL
-      //    './assets/dummy.obj',
-      //    // called when resource is loaded
-      //    function ( object ) {
+		//OBJECTS IN SCENE
+		scene.add( MiddleBulb );
+		scene.add( BottomBulb );
+		scene.add( TopBulb );
+		scene.add( pointLight );
+		scene.add( shape );
+		scene.add( floorMesh );
+		scene.add( floorMesh2 );
 
-      //       scene.add( object );
+		this.middleBulb = MiddleBulb
+		this.bottomBulb = BottomBulb
+		this.topBulb = TopBulb
+		
+		this.scene = scene
+		this.camera = camera
+		this.shape = shape
 
-      //    },
-      //    // called when loading is in progresses
-      //    function ( xhr ) {
+		//CURRENT SHAPE ROTATION
+		this.shape.shapeRotationX = this.state.shapeRotationX;
+		this.shape.shapeRotationY = this.state.shapeRotationY;
 
-      //       console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+		this.mount.appendChild(this.renderer.domElement)
+		this.start()
+	};
 
-      //    },
-      //    // called when loading has errors
-      //    function ( error ) {
+	componentWillUnmount() {
+		this.stop()
+		this.mount.removeChild(this.renderer.domElement)
+	};
 
-      //       console.log( 'An error happened' );
+	stop() {
+		cancelAnimationFrame(this.frameId)
+	};
 
-      //    }
-      // );
+	start() {
+		if (!this.frameId) {
+		  this.frameId = requestAnimationFrame(this.animate)
+		}
+	};
 
-      //OBJECTS IN SCENE
-      scene.add( MiddleBulb );
-      scene.add( BottomBulb );
-      scene.add( TopBulb );
-      scene.add( pointLight );
-      scene.add( shape );
-      scene.add( floorMesh );
-      scene.add( floorMesh2 );
+	animate() {
 
-      this.middleBulb = MiddleBulb
-      this.bottomBulb = BottomBulb
-      this.topBulb = TopBulb
-      
-      this.scene = scene
-      this.camera = camera
-      this.shape = shape
+		let middleBulbSpeed = Number(this.state.middleBulbSpeed)
+		let topBulbSpeed = Number(this.state.topBulbSpeed)
+		let bottomBulbSpeed = Number(this.state.bottomBulbSpeed)
 
-      //CURRENT SHAPE ROTATION
-      this.shape.shapeRotationX = this.state.shapeRotationX;
-      this.shape.shapeRotationY = this.state.shapeRotationY;
+		//THE DISTANCE THAT THE THINGS MOVE
+		let newShapeStateX = this.state.shapeRotationX += 0.005;
+		let newShapeStateY = this.state.shapeRotationY += 0.005;
+		let newMiddleBulbDistance = this.state.middleBulbDistance += middleBulbSpeed;
+		let newBottomBulbDistance = this.state.bottomBulbDistance += bottomBulbSpeed;
+		let newTopBulbDistance = this.state.topBulbDistance += topBulbSpeed;
 
-      //CURRENT BULB POSITION      
-      // this.middleBulb.position.x = this.state.middleBulbPositionX;
-      // this.middleBulb.position.z = this.state.middleBulbPositionZ;      
-      // this.bottomBulb.position.x = this.state.bottomBulbPositionX;
-      // this.bottomBulb.position.z = this.state.bottomBulbPositionZ;      
-      // this.topBulb.position.x = this.state.topBulbPositionX;
-      // this.topBulb.position.z = this.state.topBulbPositionZ;
+		//THE PATH THAT THE BULBS TAKE RELATED TO THE DISTANCE THEY'VE MOVED.
+		this.middleBulb.position.x = -11*Math.cos(newMiddleBulbDistance) + 0;
+		this.middleBulb.position.z = 11*Math.sin(newMiddleBulbDistance) + 0;
+		
+		this.bottomBulb.position.x = 6*Math.cos(newBottomBulbDistance) + 0;
+		this.bottomBulb.position.z = 6*Math.sin(newBottomBulbDistance) + 5;
 
-      this.mount.appendChild(this.renderer.domElement)
-      this.start()
-   };
+		this.topBulb.position.x = 7*Math.cos(newTopBulbDistance) + 0;
+		this.topBulb.position.z = 7*Math.sin(newTopBulbDistance) + 5;
 
-   componentWillUnmount() {
-      this.stop()
-      this.mount.removeChild(this.renderer.domElement)
-   };
+		let newMiddleBulbPositionX = this.middleBulb.position.x = -11*Math.cos(newMiddleBulbDistance) + 0;
+		let newMiddleBulbPositionZ = this.middleBulb.position.z = 11*Math.sin(newMiddleBulbDistance) + 0;
 
-   stop() {
-      cancelAnimationFrame(this.frameId)
-   };
+		this.setState({
+			//STORING STATE FOR THE SHAPES ROTATION DISTANCE, FOR WHEN A NEW SHAPE IS SPAWNED
+			shapeRotationX : newShapeStateX,
+			shapeRotationY : newShapeStateY,
+		}, () => {
+			this.shape.rotation.x = this.state.shapeRotationX;
+			this.shape.rotation.y = this.state.shapeRotationY;   
+		});
 
-   start() {
-      if (!this.frameId) {
-        this.frameId = requestAnimationFrame(this.animate)
-      }
-   };
+		this.renderScene()
+		this.frameId = window.requestAnimationFrame(this.animate)
+	};
 
-   animate() {
+	renderScene() {
+		this.renderer.render(this.scene, this.camera)
+	};
 
-      let middleBulbSpeed = Number(this.state.middleBulbSpeed)
-      let topBulbSpeed = Number(this.state.topBulbSpeed)
-      let bottomBulbSpeed = Number(this.state.bottomBulbSpeed)
+	componentWillReceiveProps(nextProps) {
+		const { 
+			sizeValue: sizeValueNext, 
+			color: colorNext, 
+			vertices: verticesNext, 
+			lightsColor: lightsColorNext, 
+			middleBulbSpeed: middleBulbSpeedNext,
+			topBulbSpeed: topBulbSpeedNext,
+			bottomBulbSpeed: bottomBulbSpeedNext,
+			middleBulbLum: middleBulbLumNext,
+			topBulbLum: topBulbLumNext,
+			bottomBulbLum: bottomBulbLumNext 
+		} = nextProps;
+		this.setState({ 
+			sizeValue: sizeValueNext, 
+			color: colorNext, 
+			vertices: verticesNext, 
+			lightsColor: lightsColorNext, 
+			middleBulbSpeed: middleBulbSpeedNext,
+			topBulbSpeed: topBulbSpeedNext,
+			bottomBulbSpeed: bottomBulbSpeedNext,
+			middleBulbLum: middleBulbLumNext,
+			topBulbLum: topBulbLumNext,
+			bottomBulbLum: bottomBulbLumNext 
+		}, () => this.createObject());
+	};
 
-      //THE DISTANCE THAT THE THINGS MOVE
-      let newShapeStateX = this.state.shapeRotationX += 0.005;
-      let newShapeStateY = this.state.shapeRotationY += 0.005;
-      let newMiddleBulbDistance = this.state.middleBulbDistance += middleBulbSpeed;
-      let newBottomBulbDistance = this.state.bottomBulbDistance += bottomBulbSpeed;
-      let newTopBulbDistance = this.state.topBulbDistance += topBulbSpeed;
-
-      //THE PATH THAT THE BULBS TAKE RELATED TO THE DISTANCE THEY'VE MOVED.
-      this.middleBulb.position.x = -11*Math.cos(newMiddleBulbDistance) + 0;
-      this.middleBulb.position.z = 11*Math.sin(newMiddleBulbDistance) + 0;
-      
-      this.bottomBulb.position.x = 6*Math.cos(newBottomBulbDistance) + 0;
-      this.bottomBulb.position.z = 6*Math.sin(newBottomBulbDistance) + 5;
-
-      this.topBulb.position.x = 7*Math.cos(newTopBulbDistance) + 0;
-      this.topBulb.position.z = 7*Math.sin(newTopBulbDistance) + 5;
-
-      let newMiddleBulbPositionX = this.middleBulb.position.x = -11*Math.cos(newMiddleBulbDistance) + 0;
-      let newMiddleBulbPositionZ = this.middleBulb.position.z = 11*Math.sin(newMiddleBulbDistance) + 0;
-
-      // let newBottomBulbPositionX = this.bottomBulb.position.x = 6*Math.cos(newBottomBulbDistance) + 0;
-      // let newBottomBulbPositionZ = this.bottomBulb.position.z = 6*Math.sin(newBottomBulbDistance) + 5;
-
-      // let newTopBulbPositionX = this.topBulb.position.x = 7*Math.cos(newTopBulbDistance) + 0;
-      // let newTopBulbPositionZ = this.topBulb.position.z = 7*Math.sin(newTopBulbDistance) + 5;
-
-      this.setState({
-         //STORING STATE FOR THE SHAPES ROTATION DISTANCE, FOR WHEN A NEW SHAPE IS SPAWNED
-         shapeRotationX : newShapeStateX,
-         shapeRotationY : newShapeStateY,
-
-         // middleBulbDistance : newMiddleBulbDistance,
-         // bottomBulbDistance : newBottomBulbDistance,
-         // topBulbDistance : newTopBulbDistance,
-         // middleBulbPositionX : newMiddleBulbPositionX,
-         // middleBulbPositionZ : newMiddleBulbPositionZ,   
-         // bottomBulbPositionX : newBottomBulbPositionX,
-         // bottomBulbPositionZ : newBottomBulbPositionZ,   
-         // topBulbPositionX : newTopBulbPositionX,
-         // topBulbPositionZ : newTopBulbPositionZ,
-      }, () => {
-         this.shape.rotation.x = this.state.shapeRotationX;
-         this.shape.rotation.y = this.state.shapeRotationY;   
-         // this.middleBulb.position.x = this.state.middleBulbPositionX;
-         // this.middleBulb.position.z = this.state.middleBulbPositionZ;   
-         // this.bottomBulb.position.x = this.state.bottomBulbPositionX;
-         // this.bottomBulb.position.z = this.state.bottomBulbPositionZ;   
-         // this.topBulb.position.x = this.state.topBulbPositionX;
-         // this.topBulb.position.z = this.state.topBulbPositionZ;
-      });
-
-      this.renderScene()
-      this.frameId = window.requestAnimationFrame(this.animate)
-   };
-
-   renderScene() {
-      this.renderer.render(this.scene, this.camera)
-   };
-
-   componentWillReceiveProps(nextProps) {
-      const { 
-         sizeValue: sizeValueNext, 
-         color: colorNext, 
-         vertices: verticesNext, 
-         lightsColor: lightsColorNext, 
-         middleBulbSpeed: middleBulbSpeedNext,
-         topBulbSpeed: topBulbSpeedNext,
-         bottomBulbSpeed: bottomBulbSpeedNext,
-         middleBulbLum: middleBulbLumNext,
-         topBulbLum: topBulbLumNext,
-         bottomBulbLum: bottomBulbLumNext 
-      } = nextProps;
-      this.setState({ 
-         sizeValue: sizeValueNext, 
-         color: colorNext, 
-         vertices: verticesNext, 
-         lightsColor: lightsColorNext, 
-         middleBulbSpeed: middleBulbSpeedNext,
-         topBulbSpeed: topBulbSpeedNext,
-         bottomBulbSpeed: bottomBulbSpeedNext,
-         middleBulbLum: middleBulbLumNext,
-         topBulbLum: topBulbLumNext,
-         bottomBulbLum: bottomBulbLumNext 
-      }, () => this.createObject());
-   };
-
-   render() {
-      return (
-         <div 
-            ref={(mount) => {this.mount = mount}}
-         />
-      )
-   };
+	render() {
+		return (
+			<div 
+				ref={(mount) => {this.mount = mount}}
+			/>
+		)
+	};
 }
 
 export default Shape;
-
-
-
-//OTHER CODE BITS AND PIECES
-
-// Perhaps useful?
-// shouldComponentUpdate() {
-//    return false
-// }
-
-//Perhaps useful?
-// if ((this.props.color === nextProps.color) && (this.props.sizeValue === nextProps.sizeValue) && (this.props.vertices === nextProps.vertices)) {
-//    return false;
-// }
-
-      // // instantiate a loader
-      // var loader = new THREE.OBJLoader();
-
-      // // load a resource
-      // loader.load(
-      //    // resource URL
-      //    '/assets/dummy.obj',
-      //    // called when resource is loaded
-      //    function ( shape ) {
-
-      //       scene.add( shape );
-
-      //    },
-      //    // called when loading is in progresses
-      //    function ( xhr ) {
-
-      //       console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-
-      //    },
-      //    // called when loading has errors
-      //    function ( error ) {
-
-      //       console.log( 'An error happened' );
-
-      //    }
-      // );
-
-      // let newMiddleBulbRotateY = this.middleBulb.rotation.y += 0.03;
-// let newBottomBulbRotateY = this.bottomBulb.rotation.y += 0.02;
-// let newTopBulbRotateY = this.topBulb.rotation.y -= 0.01;
-// MiddleBulbRotateY : newMiddleBulbRotateY,
-// BottomBulbRotateY : newBottomBulbRotateY,
-// TopBulbRotateY : newTopBulbRotateY,
-// this.middleBulb.rotation.y = this.state.middleBulbRotateY;
-// this.bottomBulb.rotation.y = this.state.bottomBulbRotateY;
-// this.topBulb.rotation.y = this.state.topBulbRotateY;
-
-// MiddleBulbRotateY : 0,
-// BottomBulbRotateY : 0,
-// TopBulbRotateY : 0,
-// this.middleBulb.rotation.y = this.state.middleBulbRotateY;
-// this.bottomBulb.rotation.y = this.state.bottomBulbRotateY;
-// this.topBulb.rotation.y = this.state.topBulbRotateY;
